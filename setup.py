b63352191f55084b62e68a1d7ca04e73e619b3a0
dyfsquall <dyfsquall@qq.com>
2023-10-07 18:57:01 +0800

    setup package 打包发布

---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='langchain_qianwen',
+    version='0.1.2',
+    author='Devin YF',
+    author_email='dyfsquall@qq.com',
+    description='Tongyi Qianwen Langchain adapter',
+    install_requires=[
+        'langchain==0.0.295',
+        'dashscope==1.9.0',
+    ],
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/dyfsquall/langchain_qianwen',
+    packages=find_packages(),
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
