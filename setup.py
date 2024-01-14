02c65599207e784485f36312bdb1938a40c11e9c
dyfsquall <dyfsquall@qq.com>
2024-01-14 11:04:34 +0800

    update setup.py

--- setup.py
+++ setup.py
@@ -7,12 +7,12 @@ setup(
     author_email='dyfsquall@qq.com',
     description='通义千问 Qianwen Langchain adapter',
     install_requires=[
-        'langchain>=0.0.351',
+        'langchain>=0.1.0',
         'dashscope>=1.9.0',
     ],
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
-    url='https://github.com/dyfsquall/langchain_qianwen',
+    url='https://github.com/DevinYF/langchain_qianwen',
     packages=find_packages(),
     classifiers=[
         'Programming Language :: Python :: 3',
