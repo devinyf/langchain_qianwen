2d42c8c95c34d07118f1d2eda2c457140b2b8a52
dyfsquall <dyfsquall@qq.com>
2024-01-14 11:08:00 +0800

    update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='langchain_qianwen',
-    version='0.1.13',
+    version='0.1.14',
     author='Devin YF',
     author_email='dyfsquall@qq.com',
     description='通义千问 Qianwen Langchain adapter',
